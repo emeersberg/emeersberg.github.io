900f839ef951d47647044fe990dda31f22d9c923
emeersberg <emeersberg@gmail.com>
2014-06-02 20:33:25 -0500

    re

--- soundcloud/script.js
+++ soundcloud/script.js
@@ -22,7 +22,8 @@ var getmusicgenre = function(genre) {
 	
 	SC.get('/tracks', { genres: genre }, function(tracks) {
     	$(tracks).each(function(index, track) {
-      		$('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+    		SC.oEmbed(track.permalink_url, document.getElementById('player').append());
+      		//$('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
     	});
   	});
 
