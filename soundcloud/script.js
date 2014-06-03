daee957576d4d9083be178f0d608021f11ba6e97
emeersberg <emeersberg@gmail.com>
2014-06-02 19:50:43 -0500

    dfsdfd

--- soundcloud/script.js
+++ soundcloud/script.js
@@ -7,4 +7,43 @@ $(document).ready(function() {
     SC.get("/tracks/293",function(track) {
         SC.oEmbed(track.permalink_url, document.getElementById('player'));
         })
+
+    $('#startRecording').on("click",function(){
+        $('#startRecording').hide();
+        $('#stopRecording').show();
+        SC.record({
+            progress: function(ms) {updateTimer(ms)}
+        });
+    });
+    $('#stopRecording').on("click",function(){
+        $('#startRecording').show();
+        $('#stopRecording').hide();
+        $('#upload').show();
+        SC.recordStop();
+    });
+    $('#playBack a').on("click",function(){
+        updateTimer(0);
+        SC.recordPlay({
+            progress: function(ms) {updateTimer(ms)}
+        });
+    });
+    $('#upload a').on("click",function() {
+        SC.connect({
+            connected: function() {
+                SC.recordUpload({
+                    track: {
+                        title: "My Codecademy recording",
+                        sharing: "private"
+                    }
+                })    
+            }
+        });
+    });
 });
+
+// Helper methods for our UI.
+
+function updateTimer(ms) {
+  // update the timer text. Used when we're recording
+  $('.status').text(SC.Helper.millisecondsToHMS(ms));
+}
