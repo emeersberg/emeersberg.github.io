02029833a9a88927cc5be4b74d6eda6228f7df38
emeersberg <emeersberg@gmail.com>
2014-06-02 20:20:41 -0500

    re

--- soundcloud/script.js
+++ soundcloud/script.js
@@ -4,18 +4,27 @@ SC.initialize({
 
 $(document).ready(function() {
 
+	$('.music-genre').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var genre = $(this).find("input[name='tags']").val();
+		getmusicgenre(genre);
+	});
 
     //SC.get("/tracks/293",function(track) {
     //    SC.oEmbed(track.permalink_url, document.getElementById('player'));
     //    })
 
-    SC.get('/tracks', { genres: 'industrial' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-    	SC.oEmbed(track.permalink_url, document.getElementById('player'));
-      $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-    });
-  });
-
-    
 });
+
+var getmusicgenre = function(genre) {
+	
+	SC.get('/tracks', { genres: genre }, function(tracks) {
+    	$(tracks).each(function(index, track) {
+      		$('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
+    	});
+  	});
+
+}
 
