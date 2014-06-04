22c4a3519a2cdfc6ff013b539e14c13d42d740ea
emeersberg <emeersberg@gmail.com>
2014-06-03 20:36:13 -0500

    re

--- soundcloud/script.js
+++ soundcloud/script.js
@@ -17,10 +17,16 @@ $(document).ready(function() {
       SC.oEmbed($("#inputvalue1").val(),document.getElementById('player'));
   });*/
 
-    $(this).find("a").on("click",function() {
-      alert($(this).find("#results").nearest("input[type='text']").val());
-      SC.oEmbed($(this).find("#results").nearest("input[type='text']").val(),document.getElementById('player'));
+  $(this).find("div").on("click","div",function() {
+     var test = $(this).find("input[type='text']").val();
+     SC.oEmbed(test,document.getElementById('player'));
     });
+
+
+    //$(this).find("a").on("click",function() {
+    //  alert($(this).find("#results").nearest("input[type='text']").val());
+      //SC.oEmbed($(this).find("#results").nearest("input[type='text']").val(),document.getElementById('player'));
+    //});
 
     //SC.get("/tracks/293",function(track) {
     //    SC.oEmbed(track.permalink_url, document.getElementById('player'));
@@ -33,7 +39,7 @@ var getmusicgenre = function(genre) {
 	SC.get('/tracks', { genres: genre }, function(tracks) {
     	$(tracks).each(function(index, track) {
     		SC.oEmbed(track.permalink_url, document.getElementById('player'));
-      	$('#results').append($('<a></a>').html(track.title + ' - ' + track.genre + "<input type='text' value=" + track.permalink_url + ">"));
+      	$('#results').append($('<div></div>').html(track.title + ' - ' + track.genre + "<input type='text' value=" + track.permalink_url + ">"));
     	});
   	});
 
