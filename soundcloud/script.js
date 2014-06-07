03f470fb557a94cc8de962f1266fcabb3998563b
emeersberg <emeersberg@gmail.com>
2014-06-07 12:58:28 -0500

    re

--- soundcloud/script.js
+++ soundcloud/script.js
@@ -37,7 +37,6 @@ var getmusicgenre = function(genre) {
 
   getTracks(genre);
 
-
 };
 
 var getTracks = function(genre) {
@@ -56,4 +55,43 @@ var getTracks = function(genre) {
         SC.oEmbed(firsttrack + '&auto_play=true',document.getElementById('player'));
     });
 };
+
+var getTracksTest = function(genre) {
+
+  $('#results').append($('<div id="page' + pagenumber + '"' + '></div>').html(''));
+
+  SC.get('/tracks', { genres: genre, limit: 200 }, function(tracks) 
+    {
+       $(tracks).each(function(index, track) 
+        {
+          loopcount = loopcount + 1;
+          if (loopcount === 1) 
+          {
+            firsttrack = track.permalink_url;
+          } 
+          else {}
+          if (loopcount <= 50) 
+          {
+            pagenumber = pagenumber + 1;
+          } 
+          else if (loopcount <= 100) 
+          {
+            pagenumber = pagenumber + 1;
+          }
+          else if (loopcount <= 150) 
+          {
+            pagenumber = pagenumber + 1;
+          }
+          else 
+          {
+            pagenumber = pagenumber + 1;
+          }
+          $('#page' + pagenumber).append($('<div></div>').html("<img src=" + track.artwork_url + ">" + track.title + "<input type='text' value=" + track.permalink_url + ">"));
+        });
+       SC.oEmbed(firsttrack + '&auto_play=true',document.getElementById('player'));
+       alert(pagenumber);
+       alert(loopcount);
+    });
+};
+
 
